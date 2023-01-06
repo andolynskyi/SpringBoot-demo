8cc1af4fa4d8f4544917dfa648c86ef88c55c58e
andriidolynskyi <Andriidolynskyi@gmail.com>
2023-01-05 23:45:33 -0600

    first commit

--- src/main/java/com/example/demo/student/Student.java
+++ src/main/java/com/example/demo/student/Student.java
@@ -2,13 +2,28 @@ package com.example.demo.student;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
 @Data
 @AllArgsConstructor
+@Entity
+@Table
+@NoArgsConstructor
 public class Student {
 
+    @Id
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
     private Long id;
     private String name;
     private String email;
